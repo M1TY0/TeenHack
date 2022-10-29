425a5953d564c0fca6d52760d14dc33882a8791d
M1TY0 <100305309+M1TY0@users.noreply.github.com>
2022-10-29 14:37:04 +0300

    basik html

---
+++ TeenHack/TeenHack/Pages/Ranking/Ranking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeenHack.Pages.Ranking
+{
+    public class RankingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TeenHack/TeenHack/Pages/Theachers/Teachers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeenHack.Pages.Theachers
+{
+    public class TeachersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
